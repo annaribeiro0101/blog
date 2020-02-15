069d19b24feab42e6bb5d591939898f48da7132e
unknown <Aluno@Lab-3106.labivj.info>
2020-02-14 22:34:12 -0300

    Criação de classes

---
+++ Blog/Models/Blog/Postagem/PostagemEntity.cs
@@ -0,0 +1,16 @@
+﻿using Blog.Models.Blog.Autor;
+using Blog.Models.Blog.Categoria;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Models.Blog.Postagem
+{
+    public class PostagemEntity
+    {
+        public string Titulo;
+        public AutorEntity Autor;
+        public CategoriaEntity Categoria;
+    }
+}
